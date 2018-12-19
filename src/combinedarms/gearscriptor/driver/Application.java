59593ec0360708c57b54f92b8db29722fc4ea74b
Matthew Betts <matthew.betts.96@gmail.com>
2018-12-19 21:40:37 +0000

    Simple first stage FXML UI

--- src/combinedarms/gearscriptor/driver/Application.java
+++ src/combinedarms/gearscriptor/driver/Application.java
@@ -1,9 +1,32 @@
 package combinedarms.gearscriptor.driver;
 
-public class Application
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application 
 {
+	Label lb_text;
+	Button btn_click;
+	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		//get the FXML file for this page 
+		Parent root = FXMLLoader.load(getClass().getResource("../presentation/mainScreen.fxml"));
+        //Set Title
+        stage.setTitle("CombinedArms GearScriptor");
+        //Create new scene
+        stage.setScene(new Scene(root));
+        //Screen is not resizable
+      	stage.setResizable(false);
+        stage.show();
 	}
 }
