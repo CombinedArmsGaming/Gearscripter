daa5b63c1d85cb464ab5c7997c48e193cf7f7ca7
jakdax99rb(baird) <jakdax99rb@gmail.com>
2018-12-20 14:55:52 -0500

    Changes to version location and unit tests

--- src/combinedarms/gearscriptor/tests/UpdateCheckerTests.java
+++ src/combinedarms/gearscriptor/tests/UpdateCheckerTests.java
@@ -21,9 +21,9 @@ class UpdateCheckerTests
 	
 	@Test
 	void checkForUpdateTests() throws JsonSyntaxException, Exception {
-		assertTrue(UpdateChecker.checkForUpdate(UpdateChecker.retrieveVersion() , "./src/combinedarms/gearscriptor/tests/version.json"));
+		assertTrue(UpdateChecker.checkForUpdate(UpdateChecker.retrieveVersion() , "./version.json"));
 		//This assert false claim uses 0 for both value and since that should never happen this should always return false.
-		assertFalse(UpdateChecker.checkForUpdate("[{\"releaseInfo\": {\"majorVersion\": \"0\",\"minorVersion\": \"0\",\"releaseUrl\": \"https://github.com/CombinedArmsGaming/Gearscripter/releases\"},\"licenseInfo\": {\"licenseType\": \"GPL-3.0\",\"licenseUrl\": \"https://github.com/CombinedArmsGaming/Gearscripter/blob/master/LICENSE\"}}]" , "./src/combinedarms/gearscriptor/tests/version.json"));
+		assertFalse(UpdateChecker.checkForUpdate("[{\"releaseInfo\": {\"majorVersion\": \"0\",\"minorVersion\": \"0\",\"releaseUrl\": \"https://github.com/CombinedArmsGaming/Gearscripter/releases\"},\"licenseInfo\": {\"licenseType\": \"GPL-3.0\",\"licenseUrl\": \"https://github.com/CombinedArmsGaming/Gearscripter/blob/master/LICENSE\"}}]" , "./version.json"));
 		
 	}
 
