4b542cb293079e3b529f3d6bd2c3445869a62e99
Matthew Betts <matthew.betts.96@gmail.com>
2018-12-24 19:09:13 +0000

    Added tons of UI features and interactions

--- src/combinedarms/gearscriptor/presentation/InputScreen.java
+++ src/combinedarms/gearscriptor/presentation/InputScreen.java
@@ -7,10 +7,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 public class InputScreen 
@@ -26,13 +31,35 @@ public class InputScreen
 	
 	public void buttonMainClicked()
 	{
-		//add to log - we should move this sort of thing to it's own method in the future
-		//the output box is not editable, so we need to make it so first
-		outputTextField.setEditable(true);
-		outputTextField.setText(outputTextField.getText() + "Added unit " + unitRoleBox.getText() +".\n");
-		outputTextField.setEditable(false);
+		if(unitRoleBox.getText().equals("")) 
+		{
+			popUpErrorBox("Please enter a unit role.");
+		}
+		else if(submitCodeTextField.getText().equals("")) 
+		{
+			popUpErrorBox("No code detected. Please enter arsenal code.");
+		}
+		
+		//TODO - Check if code is valid
+		else 
+		{
+			//add to log - we should move this sort of thing to it's own method in the future
+			//the output box is not editable, so we need to make it so first
+			outputTextField.setEditable(true);
+			outputTextField.setText(outputTextField.getText() + "Added unit " + unitRoleBox.getText() +".\n");
+			outputTextField.setEditable(false);
+		}
 		
 		//TODO - Pass arsenal code and unit role/name to relevant methods to parse them
+	}
+	
+	//Error box, takes a string as argument and then displays it on screen 
+	public void popUpErrorBox(String error)
+	{
+		Alert alert = new Alert(AlertType.ERROR, error, ButtonType.OK);
+		alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+		((Stage)alert.getDialogPane().getScene().getWindow()).getIcons().add(new Image(InputScreen.class.getResourceAsStream("../resources/caLogo.png")));
+	    alert.showAndWait();
 	}
 	
 	public void clearOutputBox()
@@ -48,10 +75,11 @@ public class InputScreen
 		try {
 	        ((Node)(event.getSource())).getScene().getWindow().hide();
 	        FXMLLoader fxmlLoader = new FXMLLoader();
-	        fxmlLoader.setLocation(getClass().getResource("../presentation/ViewUnits.fxml"));
+	        fxmlLoader.setLocation(getClass().getResource("../presentation/fxml/ViewUnits.fxml"));
 	        Scene scene = new Scene(fxmlLoader.load());
 	        Stage stage = new Stage();
 	        stage.setTitle("CombinedArms GearScripter");
+	        stage.getIcons().add(new Image(InputScreen.class.getResourceAsStream("../resources/caLogo.png")));
 	        stage.setScene(scene);
 	        stage.setResizable(false);
 	        stage.show();
@@ -65,10 +93,11 @@ public class InputScreen
 		try {		
 			((Node)(event.getSource())).getScene().getWindow().hide();
 	        FXMLLoader fxmlLoader = new FXMLLoader();
-	        fxmlLoader.setLocation(getClass().getResource("../presentation/LoginScreen.fxml"));
+	        fxmlLoader.setLocation(getClass().getResource("../presentation/fxml/LoginScreen.fxml"));
 	        Scene scene = new Scene(fxmlLoader.load());
 	        Stage stage = new Stage();
 	        stage.setTitle("CombinedArms GearScripter");
+	        stage.getIcons().add(new Image(InputScreen.class.getResourceAsStream("../resources/caLogo.png")));
 	        stage.setScene(scene);
 	        stage.setResizable(false);
 	        stage.show();
@@ -82,6 +111,7 @@ public class InputScreen
 	{
 		outputTextField.setTooltip(new Tooltip("General output descriptions of the program will appear here."));
 	}
+	
 	public void hoverOnTextBox() 
 	{
 		submitCodeTextField.setTooltip(new Tooltip("Enter your ACE or BI arsenal code in this box."));
--- src/combinedarms/gearscriptor/presentation/ViewUnits.java
+++ src/combinedarms/gearscriptor/presentation/ViewUnits.java
@@ -1,41 +1,76 @@
 package combinedarms.gearscriptor.presentation;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.FlowPane;
 
 public class ViewUnits 
 {
-	//TODO load only the units from the database
-	//This is currently proof of concept
 	@FXML
-	TabPane tabPane;
+	private TabPane tabPane;
+	@FXML
+	private TextArea SelectedUnitsTextView;
 	
-	// City list fetched from server
-	private String[] cityList = {"Moscow", "Stambul", "New York", "Bishkek"};
-	public void testButtonPush()
+	public void refreshUnitsViewTab() throws SQLException
 	{
-		// Add only tabs dynamically but not their content
-		for (String city : cityList) {
-		    tabPane.getTabs().add(new Tab(city));
-		}
+		//Clear all current tabs
+		tabPane.getTabs().clear();
+		
+		//Get connection to Database
+		Connection c = combinedarms.gearscriptor.driver.Application.dbConnect();
+		Statement stmt = c.createStatement();
+		
+		String sql = "SELECT * FROM UNITS";
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		while (rs.next())
+	    {
+			//Pull data from this row in the database
+			int id = rs.getInt("ID");
+			String name = rs.getString("NAME");
+			String madeby = rs.getString("MADEBY");
+			String primary = rs.getString("PRIMARYWEAPON");
+			String secondary = rs.getString("SECONDARYWEAPON");
+			
+			//Make new tab
+			Tab tab = new Tab();
+			//Set the name
+			tab.setText(name);
+		
+			//Create and populate the Textarea
+			TextArea textArea = new TextArea();
+			textArea.setText("Name : " + name + "\n" +
+			"Made by: " + madeby + "\n" +
+			"Primary: " + primary + "\n" +
+			"Secondary: " + secondary + "\n");
+			textArea.setEditable(false);
+			
+			//Create the button and link
+			Button tab_button = new Button("Add Unit");
+			tab_button.setOnAction(e -> {
+				SelectedUnitsTextView.setEditable(true);
+				SelectedUnitsTextView.setText(SelectedUnitsTextView.getText() + name + "\n");
+				SelectedUnitsTextView.setEditable(false);
+	        });
+			
+			//Make new flowpane to add both objects to the tab
+			FlowPane pane = new FlowPane();
+			pane.getChildren().addAll(textArea, tab_button);
+			tab.setContent(pane);
+			
+			tabPane.getTabs().add(tab);
+	    }
+		
+		stmt.close();
+        c.close();
 		tabPane.getSelectionModel().selectFirst();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
